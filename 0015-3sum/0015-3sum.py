bc62fb2ce995bd3ac8d6c364a5a03ee9ab8dde1e
raunak0907 <2005raunakrathor@gmail.com>
2026-08-23 13:14:43 +0530

    Time: 578 ms (75.85%), Space: 22.2 MB (82.04%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,30 +1,33 @@
-from typing import List
-
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        n = len(nums)
-        if n < 3:
-            return []
-        nums.sort()
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+
         res = []
-        for i in range(n - 2):
-            if i > 0 and nums[i] == nums[i - 1]:
+        nums.sort()
+
+        for i, a in enumerate(nums):
+
+            if i > 0 and a == nums[i - 1]:
                 continue
-            if nums[i] > 0:
-                break
-            left, right = i + 1, n - 1
-            while left < right:
-                total = nums[i] + nums[left] + nums[right]
-                if total == 0:
-                    res.append([nums[i], nums[left], nums[right]])
-                    left += 1
-                    right -= 1
-                    while left < right and nums[left] == nums[left - 1]:
-                        left += 1
-                    while left < right and nums[right] == nums[right + 1]:
-                        right -= 1
+
+            l, r = i + 1, len(nums) - 1
+
+            while l < r:
+
+                total = a + nums[l] + nums[r]
+
+                if total > 0:
+                    r -= 1
+
                 elif total < 0:
-                    left += 1
+                    l += 1
+
                 else:
-                    right -= 1
+                    res.append([a, nums[l], nums[r]])
+
+                    l += 1
+                    r -= 1
+
+                    while l < r and nums[l] == nums[l - 1]:
+                        l += 1
+
         return res
