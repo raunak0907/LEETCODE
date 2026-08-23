03f0a77d8c9acc85e3c247872e9efaf3713d815a
raunak0907 <2005raunakrathor@gmail.com>
2026-08-23 15:47:51 +0530

    Time: 448 ms (99.48%), Space: 28.6 MB (51.78%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.py
+++ 0493-reverse-pairs/0493-reverse-pairs.py
@@ -1,38 +1,8 @@
 class Solution:
     def reversePairs(self, nums: List[int]) -> int:
-
-        def merge_sort(left, right):
-            if left >= right:
-                return 0
-
-            mid = (left + right) // 2
-
-            count = merge_sort(left, mid)
-            count += merge_sort(mid + 1, right)
-
-            j = mid + 1
-
-            for i in range(left, mid + 1):
-                while j <= right and nums[i] > 2 * nums[j]:
-                    j += 1
-                count += j - (mid + 1)
-
-            temp = []
-            i, j = left, mid + 1
-
-            while i <= mid and j <= right:
-                if nums[i] <= nums[j]:
-                    temp.append(nums[i])
-                    i += 1
-                else:
-                    temp.append(nums[j])
-                    j += 1
-
-            temp.extend(nums[i:mid + 1])
-            temp.extend(nums[j:right + 1])
-
-            nums[left:right + 1] = temp
-
-            return count
-
-        return merge_sort(0, len(nums) - 1)
+        s = SortedList()
+        res = 0
+        for a in nums:
+            res += len(s) - s.bisect_right(a * 2)
+            s.add(a)
+        return res
