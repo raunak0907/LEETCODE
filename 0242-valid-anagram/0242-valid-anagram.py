90008563e5b341925abf0c700d4a41c3756ef1f8
raunak0907 <2005raunakrathor@gmail.com>
2026-08-25 22:46:47 +0530

    Time: 13 ms (51.64%), Space: 19.3 MB (76.25%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,7 +1,30 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+
+        if len(s) != len(t):
+            return False
+
+        count = {}
+
+        for c in s:
+            if c in count:
+                count[c] += 1
+            else:
+                count[c] = 1
+
+        for c in t:
+            if c not in count:
+                return False
+
+            count[c] -= 1
+
+            if count[c] < 0:
+                return False
+
+        return True
+'''   
         return Counter(s) == Counter(t)
-'''class Solution:
+class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         result = sorted(s) == sorted(t)
         print(result)
