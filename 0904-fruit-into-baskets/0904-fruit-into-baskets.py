23318a74a7555db6cfe1086ce4e91242731d98e1
raunak0907 <2005raunakrathor@gmail.com>
2026-09-09 17:39:57 +0530

    Time: 178 ms (74.73%), Space: 25.9 MB (63.88%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -0,0 +1,23 @@
+class Solution:
+    def totalFruit(self, fruits):
+        count = {}
+        left = 0
+        ans = 0
+
+        for right in range(len(fruits)):
+            count[fruits[right]] = count.get(fruits[right], 0) + 1
+
+            while len(count) > 2:
+                count[fruits[left]] -= 1
+
+                if count[fruits[left]] == 0:
+                    del count[fruits[left]]
+
+                left += 1
+
+            length = right - left + 1
+
+            if length > ans:
+                ans = length
+
+        return ans
