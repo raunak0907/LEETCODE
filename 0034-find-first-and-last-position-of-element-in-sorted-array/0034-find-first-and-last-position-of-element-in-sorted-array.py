d491d5ce069972974e5fe83d15d1580db8771089
raunak0907 <2005raunakrathor@gmail.com>
2026-09-24 18:38:18 +0530

    Time: 0 ms (100%), Space: 20.6 MB (62.87%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,31 +1,27 @@
 class Solution:
-    def searchRange(self, nums: List[int], target: int) -> List[int]:
-        def first_position():
-            l,r=0,len(nums)-1
-            ans=-1
-            while l<=r:
-                m=(l+r)//2
-                if nums[m]==target:
-                    ans=m
-                    r=m-1
-                elif nums[m]<target:
-                    l=m+1
+    def searchRange(self, nums: list[int], target: int) -> list[int]:
+        
+        # Helper function to find either the leftmost or rightmost index
+        def findBound(isFirst: bool) -> int:
+            left, right = 0, len(nums) - 1
+            bound = -1
+            
+            while left <= right:
+                mid = (left + right) // 2
+                
+                if nums[mid] == target:
+                    bound = mid
+                    if isFirst:
+                        # Found it, but keep searching to the LEFT for an earlier one
+                        right = mid - 1
+                    else:
+                        # Found it, but keep searching to the RIGHT for a later one
+                        left = mid + 1
+                elif nums[mid] < target:
+                    left = mid + 1
                 else:
-                    r=m-1
-            return ans
-
-        def last_position():
-            l,r=0,len(nums)-1
-            ans =-1
-
-            while l <=r:
-                m=(l+r)//2
-                if nums[m]==target:
-                    ans=m
-                    l=m+1
-                elif nums[m]<target:
-                    l= m+1
-                else:
-                    r=m-1
-            return ans
-        return [first_position(),last_position()]
+                    right = mid - 1
+                    
+            return bound
+            
+        return [findBound(True), findBound(False)]
